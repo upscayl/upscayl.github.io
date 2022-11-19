4697a9a11b7430e41c0f0592705455aa15b62135
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2022-11-19 18:23:16 +0530

    Changed basePath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: "/upscayl.github.io",
+  assetPrefix: "/upscayl.github.io",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
