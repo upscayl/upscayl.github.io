5fa86345a8afce403b3eab89be4fa4d1bfe4f53c
NayamAmarshe <25067102+NayamAmarshe@users.noreply.github.com>
2022-12-22 09:14:59 +0000

    Deploying to gh-pages from @ upscayl/upscayl.github.io@d3b4f02641e66d0fd64e55d57ef160922b282365 🚀

---
+++ _next/static/HnawzdhGhca3MDgEuMqLO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b4a7c617166cd25e.css","static/chunks/pages/index-2d9f22db361e189b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
