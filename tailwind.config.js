876b43aa4566b81f1bb9ebd1b6716f586cc06b17
Feenix <25067102+NayamAmarshe@users.noreply.github.com>
2023-05-10 00:53:49 +0530

    Add toast

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       keyframes: {
         "zoom-in-out": {
           "0%": { transform: "scale(100%)" },
-          "50%": { transform: "scale(120%)" },
+          "50%": { transform: "scale(105%)" },
           "100%": { transform: "scale(100%)" },
         },
       },
