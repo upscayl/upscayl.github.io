ac147a0b245dbff417a56665bb333ddfbf27ca29
NayamAmarshe <nayamamarshe@protonmail.com>
2023-05-09 19:25:09 +0000

    Deploying to gh-pages from @ upscayl/upscayl.github.io@42e829cedb09bee175cfcd140b346c68120e92af 🚀

---
+++ _next/static/SjQAXps1Vfv5SPMVK9vY4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/944-4f3d7dd0c03fded9.js","static/css/b4a7c617166cd25e.css","static/chunks/pages/index-36b70cd2ef8e3481.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
