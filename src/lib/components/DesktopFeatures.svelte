bb8fce4ad6a2c7c28a1a9753560c678718b917c9
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-08-19 19:42:02 +0530

    Add beta signup section

--- src/lib/components/DesktopFeatures.svelte
+++ src/lib/components/DesktopFeatures.svelte
@@ -1,5 +1,6 @@
 <script>
 	import IconList from './ui/IconList.svelte';
+	import Input from './ui/Input.svelte';
 </script>
 
 <div class="relative mt-40 flex min-h-screen w-full flex-row overflow-hidden px-24">
@@ -19,7 +20,30 @@
 			<IconList icon="ph:leaf" text="Free and Open Source" />
 		</div>
 	</div>
+
 	<div class="absolute -right-1/4">
 		<img src="/upscayl-desktop.png" alt="Upscayl Desktop Screenshot" class="ml-52" />
+	</div>
+</div>
+
+<div
+	class="mx-auto flex max-w-3xl flex-col gap-8 rounded-3xl bg-slate-800 px-36 py-20"
+	id="join-beta"
+>
+	<h3 class="text-center text-3xl font-medium text-slate-300">
+		Join Upscayl Pro beta for <u class="text-slate-100">free</u>
+	</h3>
+
+	<div class="flex flex-col gap-5">
+		<div class="flex flex-col gap-4">
+			<Input placeholder="Your Name" />
+			<Input placeholder="Your Email" />
+		</div>
+
+		<button
+			class="ring-ring-500 rounded-full bg-green-600 px-8 py-3 font-medium text-green-200 ring-1 ring-green-500 transition-all duration-500 hover:bg-green-500"
+		>
+			Join the waitlist
+		</button>
 	</div>
 </div>
