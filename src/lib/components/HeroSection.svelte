3c1aa8b1295ebce15076a440e565c1e4b711ead2
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-08-21 15:12:39 +0530

    Add animation

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import ring from '$lib/images/ring.png';
 	import { onMount } from 'svelte';
 	import Icon from '@iconify/svelte';
+
 	let sliderPosition = 50;
 	let container: HTMLDivElement | null = null;
 	onMount(() => {
@@ -20,21 +22,42 @@
 		<a
 			href="#cloud"
 			class="flex items-center justify-center gap-2 rounded-full bg-slate-800 px-3 py-1 text-sm text-slate-300 ring-1 ring-slate-500 transition-all duration-500 hover:bg-slate-700 sm:text-base"
+			in:fade={{
+				duration: 500
+			}}
 		>
 			<img src="/dot.svg" />
 			Introducing Upscayl Cloud <Icon icon="ph:arrow-right" width="16px" />
 		</a>
-		<h1 class="mt-16 text-5xl font-extralight text-slate-400 sm:text-center md:text-6xl">
+		<h1
+			class="mt-16 text-5xl font-extralight text-slate-400 sm:text-center md:text-6xl"
+			in:fade={{
+				duration: 500,
+				delay: 300
+			}}
+		>
 			From <span class="font-medium text-slate-200">Science Fiction</span> to
 			<span class="font-medium text-slate-200">Reality</span>
 		</h1>
-		<h4 class="text-xl font-light text-slate-400 sm:text-2xl">
+		<h4
+			class="text-xl font-light text-slate-400 sm:text-2xl"
+			in:fade={{
+				duration: 500,
+				delay: 600
+			}}
+		>
 			Supercharging your images with <span class="underline">AI</span>
 		</h4>
 		<!-- <div class="w-[30rem] rounded-xl mt-16 overflow-hidden">
 			<img src={low_res} alt="low_res" class="w-full" />
 		</div> -->
-		<div class="relative mt-10">
+		<div
+			class="relative mt-10"
+			in:fade={{
+				duration: 500,
+				delay: 900
+			}}
+		>
 			<div
 				id="slider-container"
 				class="grid place-content-center overflow-hidden rounded-2xl"
@@ -80,7 +103,13 @@
 
 	<img src={ring} alt="Ring" class="absolute left-1/2 top-32 z-0 -translate-x-1/2 md:top-28" />
 
-	<div class="mt-20 w-full">
+	<div
+		class="mt-20 w-full"
+		in:fade={{
+			duration: 500,
+			delay: 1200
+		}}
+	>
 		<p class="text-center font-bold text-slate-500">AS SEEN ON</p>
 
 		<div class="relative whitespace-nowrap p-6" id="logos-container">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,9 @@
 		});
 	}
 
+	let animate = false;
 	onMount(() => {
+		animate = true;
 		const anchors = document.querySelectorAll('a[href^="#"]');
 		anchors.forEach((anchor) => {
 			anchor.addEventListener('click', handleAnchorClick);
@@ -24,62 +26,65 @@
 	});
 </script>
 
-<div class="min-h-screen w-full bg-slate-900">
-	<Header />
+{#if animate}
+	<div class="min-h-screen w-full bg-slate-900">
+		<Header />
 
-	<main>
-		<slot />
-	</main>
+		<main>
+			<slot />
+		</main>
 
-	<footer class="mt-20 flex min-h-fit flex-col bg-slate-800 px-5 pb-4 pt-10 md:px-20">
-		<div class="flex flex-col gap-5 md:flex-row md:gap-40">
-			<div class="flex h-10 flex-row items-center justify-center gap-2">
-				<img src={logo} alt="" />
-				<h5 class="text-xl font-semibold text-slate-100">Upscayl</h5>
-			</div>
+		<footer class="mt-20 flex min-h-fit flex-col bg-slate-800 px-5 pb-4 pt-10 md:px-20">
+			<div class="flex flex-col gap-5 md:flex-row md:gap-40">
+				<div class="flex h-10 flex-row items-center justify-center gap-2">
+					<img src={logo} alt="" />
+					<h5 class="text-xl font-semibold text-slate-100">Upscayl</h5>
+				</div>
 
-			<div class="flex flex-col gap-4 p-4 text-slate-300">
-				<b>Links</b>
-				<div class="flex flex-col gap-2 text-sm">
-					<a href="https://github.com/upscayl" target="_blank" rel="noreferrer"
-						>GitHub Open Source</a
-					>
-					<a href="https://github.com/upscayl/upscayl/wiki">Wiki</a>
+				<div class="flex flex-col gap-4 p-4 text-slate-300">
+					<b>Links</b>
+					<div class="flex flex-col gap-2 text-sm">
+						<a href="https://github.com/upscayl" target="_blank" rel="noreferrer"
+							>GitHub Open Source</a
+						>
+						<a href="https://github.com/upscayl/upscayl/wiki">Wiki</a>
+					</div>
+				</div>
+
+				<div class="flex flex-col gap-4 break-all p-4 text-slate-300">
+					<b>Support</b>
+					<div class="flex flex-col gap-2 text-sm">
+						<a href="https://github.com/upscayl/upscayl/#-faq" target="_blank" rel="noreferrer"
+							>FAQ</a
+						>
+						<a href="mailto:nayam.emikx@aleeas.com"
+							>Email: <span class="text-slate-400">nayam.emikx@aleeas.com</span></a
+						>
+						<a href="https://github.com/orgs/upscayl/discussions" target="_blank" rel="noreferrer"
+							>GitHub Discussion</a
+						>
+						<a href="https://t.me/iamnayam" target="_blank" rel="noreferrer">Telegram</a>
+					</div>
 				</div>
 			</div>
 
-			<div class="flex flex-col gap-4 break-all p-4 text-slate-300">
-				<b>Support</b>
-				<div class="flex flex-col gap-2 text-sm">
-					<a href="https://github.com/upscayl/upscayl/#-faq" target="_blank" rel="noreferrer">FAQ</a
-					>
-					<a href="mailto:nayam.emikx@aleeas.com"
-						>Email: <span class="text-slate-400">nayam.emikx@aleeas.com</span></a
-					>
-					<a href="https://github.com/orgs/upscayl/discussions" target="_blank" rel="noreferrer"
-						>GitHub Discussion</a
-					>
-					<a href="https://t.me/iamnayam" target="_blank" rel="noreferrer">Telegram</a>
-				</div>
+			<hr class="mt-4 border-slate-700" />
+
+			<div class="mt-3 flex flex-row items-center justify-center text-sm text-slate-400">
+				Copyright &copy; {new Date().getFullYear()}&nbsp;
+				<a href="https://upscayl.org">Upscayl</a>
 			</div>
-		</div>
 
-		<hr class="mt-4 border-slate-700" />
-
-		<div class="mt-3 flex flex-row items-center justify-center text-sm text-slate-400">
-			Copyright &copy; {new Date().getFullYear()}&nbsp;
-			<a href="https://upscayl.org">Upscayl</a>
-		</div>
-
-		<!-- <div class="flex flex-col gap-4 p-4 text-slate-300">
+			<!-- <div class="flex flex-col gap-4 p-4 text-slate-300">
 			<b>Legal</b>
 			<div class="flex flex-col gap-2 text-sm">
 				<a href="">Privacy Policy</a>
 				<a href="">Terms of Service</a>
 			</div>
 		</div> -->
-	</footer>
-</div>
+		</footer>
+	</div>
+{/if}
 
 <style>
 	a {
