963b03af99429af1138576f2bcb9e939d5778eef
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-08-23 16:42:48 +0530

    Update og tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,28 @@
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 	<meta name="color-scheme" content="dark" />
+	<title>Upscayl - AI Image Upscaler</title>
+	<meta name="description" content="Upscayl - Free and Open Source AI Image Upscaler" />
+	<link rel="icon" type="image/png" href="/logo.png" />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="https://upscayl.org" />
+	<meta name="twitter:title" content="Upscayl - Free and Open Source AI Image Upscaler" />
+	<meta
+		name="twitter:description"
+		content="Free and Open Source AI Image Upscaler for Linux, MacOS and Windows"
+	/>
+	<meta name="twitter:image" content="https://upscayl.org/ogimage.png" />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Upscayl - Free and Open Source AI Image Upscaler" />
+	<meta
+		property="og:description"
+		content="Free and Open Source AI Image Upscaler for Linux, MacOS and Windows"
+	/>
+	<meta property="og:site_name" content="Upscayl" />
+	<meta property="og:url" content="https://upscayl.org" />
+	<meta property="og:image" content="https://upscayl.org/ogimage.png" />
 </svelte:head>
 
 <img src="/background_gradient.png" class="absolute h-full w-full" alt="background gradient" />
