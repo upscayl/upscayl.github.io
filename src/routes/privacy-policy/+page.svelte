e5478eac134c0ecd8c34f70074dd2c2e6f5634b9
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2023-10-27 22:28:33 +0530

    Update privacy pollicy

---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,45 @@
+<div class="min-h-screen w-screen">
+	<section
+		class="relative z-10 flex flex-col items-center justify-center gap-4 px-4 pt-32 sm:px-0 md:pt-28"
+	>
+		<h1 class="text-4xl font-medium text-white">Privacy Policy</h1>
+
+		<p class="max-w-2xl">
+			<b>Privacy Policy Last updated:</b> <u>27th October 2023</u>. <br /><br />
+			Your privacy is important to us. It is Upscayl's policy to respect your privacy regarding any information
+			we may collect during the use of our app, Upscayl. <br /><br />
+
+			<b>Information Collection and Use:</b><br />
+			Our app does not collect any personal information or data from users. We do not require any logins
+			or personal information for the use of our application.
+
+			<br /><br />
+
+			<b>GitHub Integration:</b><br /> Our application may use the internet to access GitHub for the
+			sole purpose of checking for new releases and updates. This interaction with GitHub is limited
+			to checking for new versions of the application and does not involve the collection, storage,
+			or processing of any personal information from the user.
+			<br /><br />
+
+			<b>External Links:</b> <br /> Our app may contain links to external sites, particularly to the
+			GitHub repository for the application. Please note that we have no control over the content
+			and practices of these sites, and we cannot accept responsibility or liability for their
+			respective privacy policies.
+			<br /><br />
+
+			<b>Data Security:</b><br />
+			We take reasonable measures to secure any data or information within our control, including protecting
+			it against unauthorized access, disclosure, or destruction.
+			<br /><br />
+
+			<b>Changes to This Privacy Policy: </b><br />
+			We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically
+			for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.
+			These changes are effective immediately after they are posted on this page. <br /><br />
+
+			<b>Contact Us:</b><br /> If you have any questions or suggestions about our Privacy Policy, do
+			not hesitate to contact us.
+			<br /><br /> This Privacy Policy applies to the use of Upscayl.
+		</p>
+	</section>
+</div>
