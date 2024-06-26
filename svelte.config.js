ae8da46e128bb151189aae5fde38ab4d592d2127
Nayam Amarshe <25067102+NayamAmarshe@users.noreply.github.com>
2024-06-26 17:57:06 +0530

    Update for cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 const dev = process.argv.includes('dev');
 
@@ -14,16 +15,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
+		// paths: {
+		// 	base: dev ? '' : process.env.BASE_PATH
+		// }
 	}
 };
 
